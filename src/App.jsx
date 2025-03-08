a3cd118e02d866beeef455c5e37990e1bf94c490
azeemhdr <comazeem@gmail.com>
2025-03-09 02:35:41 +0530

    pocket bazaar react project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h2 className='container m-auto text-center'>Welcome to Pocket Bazaar</h2>
+    </>
+  )
+}
+
+export default App
