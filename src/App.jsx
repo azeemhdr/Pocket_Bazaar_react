44e361e14666e4b24f7c2a8a8280ce8180e60160
azeemhdr <comazeem@gmail.com>
2025-03-09 17:56:49 +0530

    react router dom setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
 import Header from './components/Header'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './components/Home'
+
 function App() {
  
 
   return (
-    <>
+    <BrowserRouter>
      <Header/>
-    </>
+     <Routes>
+      <Route path='/' element={<Home/>} />
+     </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="container m-auto">
+        <h2 className="text-center my-2">Welcome to Pocket Bazaar</h2>
+        <p className="text-center">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit ipsa
+          dolore nostrum obcaecati enim et incidunt veritatis inventore sint
+          laudantium error fugit in earum, pariatur blanditiis voluptates autem
+          necessitatibus consequatur?
+        </p>
+      </div>
+    </>
+  );
+}
